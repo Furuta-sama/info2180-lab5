2f380cf750860682b9adba7397376749c619750b
Furuta-sama <tday360@gmail.com>
2020-11-19 20:21:05 -0500

    Added functionality for lookup countries

--- world.php
+++ world.php
@@ -13,46 +13,11 @@ if (isset($_GET['country'])){
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  foreach ($results as $row){
-    print($row['name'] . ' is ruled by ' . $row['head_of_state']);
-  }
-
+  echo json_encode($results);
   exit();
 }
 
-
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-/*$table = "";
-
-$table.= "<table border='1'>
-<tr>
-<th>Name</th>
-<th>Continent</th>
-<th>Independence Year</th>
-<th>Head of State</th>
-</tr>"
-
-?>
-
-<?php foreach ($results as $row):{
-  $table.=' <tr>
-  <td>'.$row["name"].'</td>
-  <td>'.$row["continent"].'</td>
-  <td>'.$row["independence_year"].'</td>
-  <td>'.$row["head_of_state"].'</td>
-  </tr>';
-
-  
-}
-endforeach;
-$table.='</table>';
-
-
-echo $table;
-*/
-
 
 echo json_encode($results);
 
