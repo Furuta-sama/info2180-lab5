89bf99904e7fcbaf82564aee5f15a0874fc6af5b
Furuta-sama <tday360@gmail.com>
2020-11-19 21:24:26 -0500

    Completed exercise 5

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     let button = document.getElementById('lookup');
+    let button2 = document.getElementById('lookup-cities');
 
     button.addEventListener('click', function(){
         var httpRequest = new XMLHttpRequest();
@@ -30,7 +31,6 @@ window.onload = function(){
                         tabCell.innerHTML = countries[i][column[j]];
                     }
                 }
-    
 
                 var result = document.getElementById('result');
                 result.appendChild(table);
@@ -44,6 +44,52 @@ window.onload = function(){
         }
         else{
             queryString = "?country=" + document.getElementById("country").value;
+        }
+
+        httpRequest.open("GET", "http://localhost:8080/world.php" + queryString, true);
+        httpRequest.send();
+    })
+
+    button2.addEventListener('click', function(){
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200) {
+                var countries = JSON.parse(this.response);
+
+                var col = ["Name", "District", "Population"];
+                var column = ["name", "district", "population"];
+
+                var table = document.createElement("table");
+                var tr = table.insertRow(-1);                   // TABLE ROW.
+
+                for (var i = 0; i < col.length; i++) {
+                    var th = document.createElement("th");      // TABLE HEADER.
+                    th.innerHTML = col[i];
+                    tr.appendChild(th);
+                }
+
+                for (var i = 0; i < countries.length; i++) {
+
+                    tr = table.insertRow(-1);
+        
+                    for (var j = 0; j < col.length; j++) {
+                        var tabCell = tr.insertCell(-1);
+                        tabCell.innerHTML = countries[i][column[j]];
+                    }
+                }
+
+                var result = document.getElementById('result');
+                result.appendChild(table);
+            }            
+        }
+        
+        var queryString = "";
+
+        if(document.getElementById("country").value == ""){
+            queryString = "";
+        }
+        else{
+            queryString = "?country=" + document.getElementById("country").value + "&context=cities";
         }
 
         httpRequest.open("GET", "http://localhost:8080/world.php" + queryString, true);
--- world.php
+++ world.php
@@ -7,15 +7,30 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
+if (isset($_GET['context'])){
+  $country = $_GET['country'];
+  if($_GET['context']=='cities'){
+      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      $code = $results[0]["code"];
+      $stmt = $conn->query("SELECT * FROM cities WHERE country_code LIKE '%$code%'");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  echo json_encode($results);
+  exit();
+}
 
 if (isset($_GET['country'])){
   $country = $_GET['country'];
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+  
   echo json_encode($results);
   exit();
 }
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
